31f418e266a2e0d67509ffece37926d6ca31b3fb
Alvin Wan <hi@alvinwan.com>
2016-01-27 01:32:13 -0800

    logout fix

--- piiipod/public/views.py
+++ piiipod/public/views.py
@@ -118,7 +118,8 @@ def request_loader(request):
 @app.route('/logout')
 def logout():
     flask_login.logout_user()
-    return redirect(url_for('public.home', logout='true'))
+    return redirect(request.args.get('redirect',
+        url_for('public.home')) + '?logout=true')
 
 @login_manager.unauthorized_handler
 def unauthorized_handler():
